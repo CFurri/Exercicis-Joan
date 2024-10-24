f8a4c91fa1b58f67a31ee381e8722d7b39a4e27c
Carles Furriols Furriols <carles.furriols@uvic.cat>
2024-10-24 19:58:33 +0200

    Batman

--- exercici_llistaJoan_5.py
+++ exercici_llistaJoan_5.py
@@ -1,10 +1,14 @@
 #J-5:
+
 compt = 0
 sumatori = 0
-serie = -1
+serie = 0
+
 while serie != 0:
+    serie = int(input("Número: "))
     sumatori = sumatori + serie
     compt = compt + 1
-    serie = int(input("Número: "))
+    
     
 print(sumatori/compt)
+
